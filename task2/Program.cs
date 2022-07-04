dde23eb419003a399479fe9644afd6d61b5bf447
NataliyaDobr <n.m.suvorova@mail.ru>
2022-07-04 21:40:06 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int number = Prompt("Введите число  ");
+
+int sumNumber(int A)
+{
+    int res = 0;
+    while (A > 0)
+    {
+        res += A % 10;
+        A = A / 10;
+    }
+    return res;
+}
+
+int result = sumNumber(number);
+System.Console.WriteLine($"Сумма цифр числа {number} равна {result}");
