660a867bc6db62d47f31c90a2f1b408797e6824f
NataliyaDobr <n.m.suvorova@mail.ru>
2022-07-04 21:47:13 +0300

    test3

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int RandomNumb(int min, int max)
+{
+    int a = new Random().Next(min, max + 1);
+    return a;
+}
+
+int[] NewArray(int length, int min, int max)
+{
+    int[] arr = new int[length];
+    int i = 0;
+    while (i < length)
+    {
+        arr[i] = RandomNumb(min, max);
+        i++;
+    }
+    return arr;
+}
+
+void Print(int[] array)
+{
+    System.Console.Write("[");
+    System.Console.Write(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write(", " + array[i]);
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] newArray = NewArray(8, 0, 9);
+Print(newArray);
